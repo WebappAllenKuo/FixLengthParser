51562484766d4ac88f938d2b46001d59c04c5079
Allen Kuo <g01.webapp@gmail.com>
2020-03-26 10:07:26 +0800

    sample

---
+++ ParseString.Solution/WA.StringParser.Tests/FixLengthParserTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace WA.StringParser.Tests
+{
+    [TestFixture]
+    public class FixLengthParserTests
+    {
+	    [TestCase(null)]
+	    [TestCase("")]
+	    [TestCase(" ")]
+		public void ParseSingleLine_空字串_傳回空集合(string source)
+		{
+			int[] columnLengths = {7,3 };
+			var result = FixLengthParser.ParseSingleLine(source, columnLengths);
+
+			result.Should().BeEmpty();
+		}
+
+		[TestCase("ab")]
+		[TestCase("abc")]
+		[TestCase("abcd")]
+		public void ParseSingleLine_純英文_文字長度不拘都能解析(string source)
+		{
+			int[] columnLengths = { 3, 5 };
+			int expectedColumns = columnLengths.Length;
+			var result = FixLengthParser.ParseSingleLine(source, columnLengths);
+
+			result.Should().HaveCount(c => c == expectedColumns);
+		}
+
+		[Test]
+		public void ParseSingleLine_純英文_驗證可解析成2欄()
+		{
+			string source = "abcd";
+			int[] columnLengths = { 3, 5 };
+			
+			var result = FixLengthParser.ParseSingleLine(source, columnLengths);
+
+			result[0].Should().Be("abc");
+			result[1].Should().Be("d");
+
+		}
+
+		[Test]
+		public void ParseSingleLine_純中文_驗證可解析成2欄()
+		{
+			string source = "王小明李小華";
+			int[] columnLengths = { 6, 4 };
+
+			var result = FixLengthParser.ParseSingleLine(source, columnLengths);
+
+			result[0].Should().Be("王小明");
+			result[1].Should().Be("李小");
+
+		}
+
+		[Test]
+		public void ParseSingleLine_中英文混合_驗證可解析成2欄()
+		{
+			string source = "王abc123";
+			int[] columnLengths = { 6, 4 };
+
+			var result = FixLengthParser.ParseSingleLine(source, columnLengths);
+
+			result[0].Should().Be("王abc1");
+			result[1].Should().Be("23");
+		}
+
+		[Test]
+		public void ParseBig5File_WhenCalled()
+		{
+			string filePath = @"D:\temp\big5.txt";
+			int[] columnLengths = { 7, 3 };
+			var result = FixLengthParser.ParseBig5File(filePath, columnLengths);
+
+			result.Should().HaveCount(c => c == 2);
+
+			result[0][0].Should().Be("中文eng");
+			result[0][1].Should().Be("123");
+
+			result[1][0].Should().Be("abcd   ");
+			result[1][1].Should().Be("456");
+		}
+	}
+}
---
+++ ParseString.Solution/WA.StringParser/FixLengthParser.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WA.StringParser
+{
+    /// <summary>
+    /// 將一行big5編碼的文字, 用固定長度切割
+    /// </summary>
+    public static class FixLengthParser
+    {
+        /// <summary>
+        /// 解析 Big5編碼的純文字檔案, 並逐行依指定長度解析
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="columnLengths"></param>
+        /// <returns></returns>
+	    public static List<string[]> ParseBig5File(string filePath, int[] columnLengths)
+	    {
+		    if (string.IsNullOrWhiteSpace(filePath)) throw new ArgumentNullException(nameof(filePath));
+		    if (!System.IO.File.Exists(filePath)) throw new FileNotFoundException(nameof(filePath)); 
+		    
+		    if (columnLengths == null) throw new ArgumentNullException(nameof(columnLengths));
+            if (columnLengths.Length == 0) throw new ArgumentOutOfRangeException(nameof(columnLengths));
+            if(columnLengths.Any(item => item == 0)) throw new ArgumentOutOfRangeException($"{nameof(columnLengths)} 裡的長度不可以小於等於零");
+            
+            var result = new List<string[]>();
+            foreach (var singleLine in System.IO.File.ReadAllLines(filePath))
+            {
+	         result.Add(ParseSingleLine(singleLine, columnLengths));   
+            }
+
+            return result;
+	    }
+
+        /// <summary>
+        /// 將一段 Big5編碼的字串,根據各欄位bytes數量拆解成多個欄位
+        /// </summary>
+        /// <param name="columnLengths"></param>
+        /// <returns></returns>
+	    public static string[] ParseSingleLine(string source, int[] columnLengths)
+	    {
+            if(string.IsNullOrWhiteSpace(source)) return new string[0];
+
+            byte[] bytes = Encoding.GetEncoding(950).GetBytes(source);
+
+            var result = new string[columnLengths.Length];
+            int index = 0;
+            int startIndex = 0;
+            
+            foreach (var columnLength in columnLengths)
+            {
+                if(startIndex > bytes.Length-1)break;
+                
+	            int endIndex =Math.Min( startIndex + columnLength, bytes.Length)-1;
+	            string columnValue = Encoding.GetEncoding(950).GetString(bytes, startIndex, endIndex - startIndex + 1);
+
+	            result[index] = columnValue;
+
+	            startIndex += columnLength;
+	            index++;
+            }
+
+            return result;
+	    }
+    }
+}
